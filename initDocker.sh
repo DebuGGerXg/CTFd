471780ae3e7a00774006c7c22de7db33512c06cf
xg <2412650616@qq.com>
2023-03-16 21:22:46 +0800

    change compose install sh

--- initDocker.sh
+++ initDocker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 curl -sSL https://get.daocloud.io/docker | sh
-curl -L https://get.daocloud.io/docker/compose/releases/download/v2.4.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 docker-compose version
