d67fd4cd5f7bbbf6873036a5fa6774de338f76a4
Two B smaller <62636735+XG3512@users.noreply.github.com>
2023-08-29 19:42:08 +0800

    Update initDocker.sh

--- initDocker.sh
+++ initDocker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-curl -sSL https://get.daocloud.io/docker | sh
+curl -fsSL https://test.docker.com -o test-docker.sh
+sudo sh test-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.2.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
